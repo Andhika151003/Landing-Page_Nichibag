f9e29dba5a78f296b2ae526e3f096abcbde832e5
affirnanda <ahmadnanda402@gmail.com>
2025-08-03 19:20:58 +0700

    baru caorusol

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Navbar from "./frontend/navbar.jsx"; 
+import Hero from "./frontend/Hero.jsx";
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Hero />} />
       </Routes>
     </Router>
   );
