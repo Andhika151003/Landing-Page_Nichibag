e6dcf09ee80cebe77a7a87c75069db3e5178dd65
duriskifeb <rizkydurfeb976@gmail.com>
2025-08-24 02:11:13 +0700

    fix test postman user

--- Server/index.js
+++ Server/index.js
@@ -9,17 +9,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Koneksi ke MongoDB
-mongoose.connect("mongodb://localhost:27017/nichibag_db", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect("mongodb://localhost:27017/nichibag_db");
 
 const db = mongoose.connection;
+
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log(" Connected to database"));
 
+// Middleware harus sebelum route
 app.use(cors());
 app.use(express.json());
-app.use(UserRoute)
+
+app.use(UserRoute);
+app.use(ProductRoute);
+app.use(KelolaRoute);
 
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
