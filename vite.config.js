d9f98f17cf145cbcc6d43ab477f285179a9608d5
Andhika Pratama Adhi Surya <andhikapratama1510@gmail.com>
2025-12-10 14:04:29 +0700

    test vite config

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
+  server: {
+    host: '127.0.0.1', // Paksa menggunakan IPv4 agar cocok dengan wait-on di CI
+    port: 5173, // Pastikan port selalu 5173  
+  }             
 })
